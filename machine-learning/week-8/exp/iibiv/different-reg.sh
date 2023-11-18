0f5a4d6ce10e82f7f75fc644ddab587535fed963
Neimhin Christopher Robinson Gunning <nrobinso@tcd.ie>
2023-11-18 11:36:01 +0000

    use reg variable stupid bug

--- machine-learning/week-8/exp/iibiv/different-reg.sh
+++ machine-learning/week-8/exp/iibiv/different-reg.sh
@@ -7,7 +7,7 @@ for reg in 0.0 0.000001 0.00001 0.001 0.01 0.1 1 10 100 1000; do
 			--output-history-csv $pref.history.csv \
 			--save-model-to $pref.model.h5 \
 			--save-fit-time $pref.time.txt \
-			--l1-reg 0.001
+			--l1-reg $reg
 	fi
 	
 	python  src/iibiii_plot.py \
